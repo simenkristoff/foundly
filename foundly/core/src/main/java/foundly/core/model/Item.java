cc9cbb14e8cc55fd3087abead2d128ed7c699283
Lea Haug Sandberg <leahs@stud.ntnu.no>
2020-10-29 17:43:41 +0000

    Fått item til å samsvare med restAPI. Slettet model-klassen

--- foundly/core/src/main/java/foundly/core/model/Item.java
+++ foundly/core/src/main/java/foundly/core/model/Item.java
@@ -1,304 +1,170 @@
 package foundly.core.model;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Blob;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Date;
+import java.time.LocalDateTime;
 
-import javax.sql.rowset.serial.SerialBlob;
+import javax.persistence.*;
 
-import javafx.scene.image.Image;
-
-/**
- * The Class Item.
- * Model-object used to insert and retrieve data
- * from database
- */
-public class Item extends Model{
-
-	@DBTable(columnName="id", func="getId")
-	protected Integer id;
-	
-	@DBTable(columnName="state", func="getStateAsString")
-	protected State state;
-	
-	@DBTable(columnName="title", func="getTitle")
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+ 
+@Entity
+@Table(name = "items")
+public class Item {
+	 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	  
+	@Column(name = "title")
 	private String title;
-	
-	@DBTable(columnName="description", func="getDescription")
+	  
+	@Column(name = "description")
 	private String description;
-	
-	@DBTable(columnName="image", func="getImageBlob")
-	private Blob imageBlob;
-	
-	@DBTable(columnName="created_at", func="getDateCreated")
-	private Date dateCreated;
-	
-	private Image image;
-	
-	
-	/**
-	 * Instantiates a new item.
-	 *
-	 * @param id the id
-	 * @param title the title
-	 * @param description the description
-	 */
-	public Item(Integer id, State state, String title, String description) {
-		this.id = id;
-		this.state = state;
+	  
+	@Enumerated(EnumType.STRING)
+	private State state;
+	  
+	@Column(name = "email")
+	private String email;
+	  
+	@Column(name = "phone")
+	private String phone;
+	  
+	@Column(name = "image")
+	private String image;
+	  
+	@Column(name = "date")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime date;
+	 
+	public Item() {}
+
+	public Item(String title, String description, State state, String email, String phone, String image) {
 		this.title = title;
 		this.description = description;
-		this.imageBlob = null;
-		this.dateCreated = new Date();
-	}
-	
-	/**
-	 * Instantiates a new item.
-	 *
-	 * @param id the id
-	 * @param title the title
-	 * @param description the description
-	 * @param imageBlob the image blob
-	 */
-	public Item(Integer id, State state, String title, String description, Blob imageBlob) {
-		this.id = id;
 		this.state = state;
+		this.email = email;
+		this.phone = phone;
+		this.image = image;
+	}
+  
+	public Item(String title, String description, State state, String email, String phone, String image, LocalDateTime date) {
 		this.title = title;
 		this.description = description;
-		this.imageBlob = imageBlob;
-		this.dateCreated = new Date();
-	}
-	
-	
-	/**
-	 * Instantiates a new item.
-	 *
-	 * @param id the id
-	 * @param title the title
-	 * @param description the description
-	 * @param image the image
-	 */
-	public Item(Integer id, State state, String title, String description, InputStream inputStream) {
-		this.id = id;
 		this.state = state;
-		this.title = title;
-		this.description = description;
-		try {
-			this.imageBlob = new SerialBlob(inputStream.readAllBytes());}
-			catch (SQLException | IOException e) {this.imageBlob = null; e.printStackTrace();
-		}
-		this.dateCreated = new Date();
+		this.email = email;
+		this.phone = phone;
+		this.image = image;
+		this.date = date;
 	}
-
-	/**
-	 * Instantiates a new item.
-	 *
-	 * @param rs the rs
-	 * @throws SQLException the SQL exception
-	 */
-	public Item(ResultSet rs) throws SQLException {
-		readStateFromRs(rs);
-		this.id = rs.getInt("id");
-		this.title = rs.getString("title");
-		this.description = rs.getString("description");
-		this.imageBlob = rs.getBlob("image");
-		this.dateCreated = rs.getTimestamp("created_at");
+	  
+	public long getId() {
+		return id;
 	}
-
-	/**
-	 * Gets the id.
-	 *
-	 * @return the id
-	 */
-	public Integer getId() {
-		return this.id;
-	}
-
-	/**
-	 * Sets the id.
-	 *
-	 * @param id the new id
-	 */
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	
-	/**
-	 * Gets the state as a String.
-	 *
-	 * @return the state as String
-	 */
-	public String getStateAsString() {
-		return this.state.toString();
-	}
-	
-	/**
-	 * Gets the state.
-	 *
-	 * @return the state
-	 */
-	public State getState() {
-		return this.state;
-	}
-
-	/**
-	 * Sets the state.
-	 *
-	 * @param state the new state
-	 */
-	public void setState(State state) {
-		this.state = state;
-	}
-
-	/**
-	 * Gets the title.
-	 *
-	 * @return the title
-	 */
-	public String getTitle() {
-		return this.title;
-	}
-
-	/**
-	 * Sets the title.
-	 *
-	 * @param title the new title
-	 */
+	 
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
-	/**
-	 * Gets the description.
-	 *
-	 * @return the description
-	 */
-	public String getDescription() {
-		return this.description;
+	 
+	public String getTitle() {
+		return this.title;
 	}
-
-	/**
-	 * Sets the description.
-	 *
-	 * @param description the new description
-	 */
+	  
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	/**
-	 * Gets the image blob.
-	 *
-	 * @return the image blob
-	 */
-	public Blob getImageBlob() {
-		return this.imageBlob;
+	 
+	public String getDescription() {
+		return this.description;
 	}
-
-	/**
-	 * Sets the image blob.
-	 *
-	 * @param imageBlob the new image blob
-	 */
-	public void setImageBlob(Blob imageBlob) {
-		this.imageBlob = imageBlob;
+	 
+	public State getState() {
+		return this.state;
 	}
-	
-	/**
-	 * Gets the image.
-	 *
-	 * @return the image
-	 */
-	public Image getImage() {
-		if(this.image == null && this.imageBlob != null) {
-			try {
-				this.image = new Image(this.imageBlob.getBinaryStream());
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+	 
+	public void setState(State state) {
+		this.state = state;
+	}
+	  
+	public void setImage(String image) {
+		this.image = image;
+	}
+	  
+	public String getEmail() {
+		return this.email;
+	}
+	  
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	  
+	public String getPhone() {
+		return this.phone;
+	}
+	  
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	  
+	public String getImage() {
 		return this.image;
 	}
-	
-	/**
-	 * Gets the date the item was created.
-	 *
-	 * @return the date of creation
-	 */
-	public Date getDateCreated() {
-		return new Date(dateCreated.getTime());
+	  
+	public void setDate(LocalDateTime date) {
+		this.date = date;
 	}
-
-	/**
-	 * To string.
-	 *
-	 * @return the string
-	 */
+	  
+	@JsonValue
+	public LocalDateTime getDate() {
+		return this.date;
+	}
+	 
 	@Override
 	public String toString() {
-		return "Item [id=" + id + ", state=" + state +", title=" + title + ", image=" + imageBlob + "]";
+		return "Item [id=" + id + ", title=" + title + ", description=" + description + ", state=" + state + ", email=" + email + ", phone=" + phone + ", image=" + image + ", date=" + date + "]";
 	}
-	
-	
-	/**
-	 * Sets the correct State of Items stored in database
-	 *
-	 * @param rs the Item fetched from database
-	 *
-	 * @throws SQLException 
-	 */
-	private void readStateFromRs(ResultSet rs) throws SQLException {
-		String state = rs.getString("state");
-		switch (state) {
-			case "FOUND":
-				this.state = State.FOUND;
-				break;
-			case "LOST":
-				this.state = State.LOST;
-				break;
-			default:
-				this.state = State.FOUND;
-				break;
-		}	
-	}
-	
+	  
 	public enum State {
-		LOST("MISTET") {
-			public String toString() {
-				return "LOST";
-			}
-		},
-		FOUND("FUNNET"){
-			public String toString() {
-				return "FOUND";
-			}
-		};
-		
+		@JsonProperty("LOST")
+		LOST("MISTET"),
+		@JsonProperty("FOUND")
+		FOUND("FUNNET");
+		  
 		private String value;
-		
-		
+			
 		/**
 		 * Instantiates a new State.
 		 *
 		 * @param value the string value of the state
 		 */
-		private State(final String value) {
+		private State(String value) {
 			this.value = value;
 		}
 		
-	    /**
+		/**
 		 * Gets the value
 		 * 
 		 * @return the value
 		 */
+		@JsonValue
 		public String getValue() {
 			return value;
 		}
 		
-		
+		@JsonCreator
+		public static State fromString(String stateStr) {
+			if(stateStr != null) {
+				for(State state : State.values()) {
+					if(stateStr.equalsIgnoreCase(state.toString())) {
+						return state;
+					}
+				}
+			}
+			return null;
+		}
 	}
-	
 }
