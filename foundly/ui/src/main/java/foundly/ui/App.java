f0e588428cc03f19bded3c6a036906f3cdcb64e9
Simen Kristoffersen <simekri@stud.ntnu.no>
2020-10-30 14:52:42 +0100

    Fikset imports i foundly.ui.App.java:
    * La til imports som ikke kom med i issue #43.

--- foundly/ui/src/main/java/foundly/ui/App.java
+++ foundly/ui/src/main/java/foundly/ui/App.java
@@ -7,10 +7,13 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
 
+import foundly.restapi.RestApi;
 import foundly.ui.controller.Navigator;
 
 /**
@@ -18,28 +21,28 @@ import foundly.ui.controller.Navigator;
  */
 @SpringBootApplication
 public class App extends Application {
-    
-    private ConfigurableApplicationContext springContext;
+
+	private ConfigurableApplicationContext springContext;
 	/** The title. */
 	private final String TITLE = "Foundly";
-	
+
 	/** The width. */
 	private final double WIDTH = 720;
-	
+
 	/** The height. */
 	private final double HEIGHT = 640;
-	
+
 	private final int COUNT_LIMIT = 10;
-	
+
 	/** The icon **/
 	private final Image ICON = new Image(App.class.getResource("img/icons/icon.png").toExternalForm());
-	
+
 	/** The logo **/
 	public static final Image LOGO = new Image(App.class.getResource("img/logo.png").toExternalForm());
-	
+
 	/** The navigator. */
 	private Navigator navigator;
-	
+
 	/**
 	 * Start the app.
 	 *
@@ -56,52 +59,50 @@ public class App extends Application {
 		stage.setResizable(true);
 		stage.centerOnScreen();
 		stage.show();
-		
+
 		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 			@Override
 			public void handle(WindowEvent event) {
 				try {
-                    stop();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+					stop();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
-    }
-    
-    @Override
-    public void stop() throws Exception {
-            springContext.close();
-            System.exit(-1);
-        }
-	
-    /**
-     * Initialize app.
-     */
-    public void init() throws Exception {
-        this.springContext = SpringApplication.run(RestApi.class);
-    	this.navigator = new Navigator();
-    	for(int i = 0; i < COUNT_LIMIT; i++) {
-    		double progress = (double) i / COUNT_LIMIT;
-    		this.notifyPreloader(new Preloader.ProgressNotification(progress));
-    		Thread.sleep(100);
-    	}
-    }
-    
+	}
 
-    
-    public Navigator getNavigator() {
-    	return this.navigator;
-    }
+	@Override
+	public void stop() throws Exception {
+		springContext.close();
+		System.exit(-1);
+	}
 
-    /**
-     * The main method.
-     *
-     * @param args the arguments
-     */
-    public static void main(String[] args) {
-        System.setProperty("javafx.preloader", SplashScreen.class.getCanonicalName());
-        launch();
-    }
+	/**
+	 * Initialize app.
+	 */
+	public void init() throws Exception {
+		this.springContext = SpringApplication.run(RestApi.class);
+		this.navigator = new Navigator();
+		for (int i = 0; i < COUNT_LIMIT; i++) {
+			double progress = (double) i / COUNT_LIMIT;
+			this.notifyPreloader(new Preloader.ProgressNotification(progress));
+			Thread.sleep(100);
+		}
+	}
+
+	public Navigator getNavigator() {
+		return this.navigator;
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		System.setProperty("javafx.preloader", SplashScreen.class.getCanonicalName());
+		launch();
+	}
 
 }
