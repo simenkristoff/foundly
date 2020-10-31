21a822b6486b91f137c1f2a73683f140ca268235
Simen Kristoffersen <simekri@stud.ntnu.no>
2020-10-31 12:28:01 +0000

    http-commons endrer url-base i GitPod
    La til en test som sjekker url-basen, og endrer
    denne hvis vi ikke bruker localhost

--- foundly/web-client/src/http-common.js
+++ foundly/web-client/src/http-common.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-let api_url = 'http://localhost:8080';
+let api_url = 'http://localhost:8080/';
+
+/** 
+ * Fix for sending requests in GitPod.
+ * Changes the base url to GitPod's dynamic URL. 
+ * The proxy in vue.config.js will take care of the port-isssue.
+ */
+api_url === location.href ? api_url : location.href;
 
 export default axios.create({
   baseURL: api_url + '/api',
