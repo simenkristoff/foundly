115162f800a3217a678e7a329d5edefb39f4038e
Simen Kristoffersen <simekri@stud.ntnu.no>
2020-11-01 19:52:04 +0100

    Fikset checkstyle-feil i ui
    * Reformatterte all kode
    
    * Endret rekkefølgen på imports
    
    * Slettet ubrukt kode
    
    * Endret navn på variabler slik at de samsvarer med checkstyle

--- foundly/ui/src/main/java/foundly/ui/App.java
+++ foundly/ui/src/main/java/foundly/ui/App.java
@@ -1,20 +1,18 @@
 package foundly.ui;
 
+import foundly.restapi.RestApi;
+import foundly.ui.controller.Navigator;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 import javafx.application.Application;
 import javafx.application.Preloader;
 import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.io.IOException;
-
-import foundly.restapi.RestApi;
-import foundly.ui.controller.Navigator;
 
 /**
  * JavaFX App.
@@ -22,94 +20,78 @@ import foundly.ui.controller.Navigator;
 @SpringBootApplication
 public class App extends Application {
 
-	private ConfigurableApplicationContext springContext;
-	/** The title. */
-	private final String TITLE = "Foundly";
+  public static final DateTimeFormatter DATEPATTERN = DateTimeFormatter.ofPattern("dd.MM.YY HH:mm");
+  public static final String API_URL = "http://localhost:8098";
 
-	/** The width. */
-	private final double WIDTH = 720;
+  private static final String TITLE = "Foundly";
+  private static final double WIDTH = 720;
+  private static final double HEIGHT = 640;
+  private static final int COUNT_LIMIT = 10;
+  private static final String ICON = "img/icons/icon.png";
 
-	/** The height. */
-	private final double HEIGHT = 640;
+  private ConfigurableApplicationContext springContext;
+  private Navigator navigator;
 
-	private final int COUNT_LIMIT = 10;
+  /**
+   * Start the app.
+   *
+   * @param stage the stage
+   * @throws IOException Signals that an I/O exception has occurred.
+   */
+  public void start(Stage stage) throws IOException {
+    stage.getIcons().add(new Image(this.getClass().getResource(ICON).toExternalForm()));
+    stage.setTitle(TITLE);
+    stage.setScene(this.navigator.getScene());
+    stage.setWidth(WIDTH);
+    stage.setHeight(HEIGHT);
+    stage.setMaximized(false);
+    stage.setResizable(true);
+    stage.centerOnScreen();
+    stage.show();
 
-	/** The icon **/
-	private final Image ICON = new Image(App.class.getResource("img/icons/icon.png").toExternalForm());
+    stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+      @Override
+      public void handle(WindowEvent event) {
+        try {
+          stop();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
 
-	/** The logo **/
-	public final Image LOGO = new Image(App.class.getResource("img/logo.png").toExternalForm());
+  @Override
+  public void stop() throws Exception {
+    springContext.close();
+    System.exit(-1);
+  }
 
-	/** The navigator. */
-	private Navigator navigator;
-	
-	/** BASE-URL for the API-services **/
-	private final static String API_URL = "http://localhost:8098";
-	
-	/**
-	 * Start the app.
-	 *
-	 * @param stage the stage
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 */
-	public void start(Stage stage) throws IOException {
-		stage.getIcons().add(ICON);
-		stage.setTitle(TITLE);
-		stage.setScene(this.navigator.getScene());
-		stage.setWidth(WIDTH);
-		stage.setHeight(HEIGHT);
-		stage.setMaximized(false);
-		stage.setResizable(true);
-		stage.centerOnScreen();
-		stage.show();
+  /**
+   * Initialize app.
+   */
+  public void init() throws Exception {
+    this.springContext = SpringApplication.run(RestApi.class);
+    this.navigator = new Navigator();
+    for (int i = 0; i < COUNT_LIMIT; i++) {
+      double progress = (double) i / COUNT_LIMIT;
+      this.notifyPreloader(new Preloader.ProgressNotification(progress));
+      Thread.sleep(100);
+    }
+  }
 
-		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-			@Override
-			public void handle(WindowEvent event) {
-				try {
-					stop();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+  public Navigator getNavigator() {
+    return this.navigator;
+  }
 
-	@Override
-	public void stop() throws Exception {
-		springContext.close();
-		System.exit(-1);
-	}
-
-	/**
-	 * Initialize app.
-	 */
-	public void init() throws Exception {
-		this.springContext = SpringApplication.run(RestApi.class);
-		this.navigator = new Navigator();
-		for (int i = 0; i < COUNT_LIMIT; i++) {
-			double progress = (double) i / COUNT_LIMIT;
-			this.notifyPreloader(new Preloader.ProgressNotification(progress));
-			Thread.sleep(100);
-		}
-	}
-
-	public Navigator getNavigator() {
-		return this.navigator;
-	}
-	
-	public static String getApiUrl() {
-		return API_URL;
-	}
-
-	/**
-	 * The main method.
-	 *
-	 * @param args the arguments
-	 */
-	public static void main(String[] args) {
-		System.setProperty("javafx.preloader", SplashScreen.class.getCanonicalName());
-		Application.launch(args);
-	}
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+    System.setProperty("javafx.preloader", SplashScreen.class.getCanonicalName());
+    Application.launch(args);
+  }
 
 }
--- foundly/ui/src/main/java/foundly/ui/controller/SplashController.java
+++ foundly/ui/src/main/java/foundly/ui/controller/SplashController.java
@@ -2,70 +2,72 @@ package foundly.ui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 
 /**
- * The Class SplashController.
- * Controls the Preloader.
+ * The Class SplashController. Controls the Preloader.
  */
 public class SplashController implements Initializable {
 
-	private static Label label;
-	private static ProgressBar progress;
-	
-    @FXML
-    private Label splash_txt;
-    
-    @FXML
-    private ProgressBar splash_progress;
+  private static Label label;
+  private static ProgressBar progress;
 
-	/**
-	 * Initialize.
-	 *
-	 * @param location the location
-	 * @param resources the resources
-	 */
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		setLabel(splash_txt);
-		setProgressBar(splash_progress);
-	}
-	
-	/**
-	 * Thread safe function to set the Label
-	 * @param label the Label to be set
-	 */
-	public synchronized static void setLabel(Label label) {
-		SplashController.label = label;
-	}
-	
-	/**
-	 * Sets the text of label
-	 * @param text
-	 */
-	public static void setLabelText(String text) {
-		label.setText(text);
-	}
-	
-	/**
-	 * Thread safe function to set the ProgressBar
-	 * @param progressBar the ProgressBar to be set
-	 */
-	public synchronized static void setProgressBar(ProgressBar progressBar) {
-		SplashController.progress = progressBar;
-	}
-	
-	/**
-	 * Sets the progress value
-	 * @param value the progress
-	 */
-	public static void setProgressValue(Double value) {
-		progress.setProgress(value);
-	}
+  @FXML
+  private Label splashText;
+
+  @FXML
+  private ProgressBar splashProgress;
+
+  /**
+   * Initialize.
+   *
+   * @param location the location
+   * @param resources the resources
+   */
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    setLabel(splashText);
+    setProgressBar(splashProgress);
+  }
+
+  /**
+   * Thread safe function to set the Label.
+   * 
+   * @param label the Label to be set
+   */
+  public static synchronized void setLabel(Label label) {
+    SplashController.label = label;
+  }
+
+  /**
+   * Sets the text of label.
+   * 
+   * @param text the label text
+   */
+  public static void setLabelText(String text) {
+    label.setText(text);
+  }
+
+  /**
+   * Thread safe function to set the ProgressBar.
+   * 
+   * @param progressBar the ProgressBar to be set
+   */
+  public static synchronized void setProgressBar(ProgressBar progressBar) {
+    SplashController.progress = progressBar;
+  }
+
+  /**
+   * Sets the progress value.
+   * 
+   * @param value the progress
+   */
+  public static void setProgressValue(Double value) {
+    progress.setProgress(value);
+  }
 
 
 }
