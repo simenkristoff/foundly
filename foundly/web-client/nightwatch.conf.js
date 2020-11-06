d4d729d5caebf02b7d25dbbbaf11301997528375
Simen Kristoffersen <simekri@stud.ntnu.no>
2020-11-06 23:34:49 +0100

    Fikset chromedriver-feil ved lokal kompilering
    Fikk feil av typen: 'An error occurred while trying to start
    ChromeDriver: cannot resolve path: "/node_modules/.bin/chromedriver"'
    
    Det viser seg at chromedriver ikke regnes som en dependency når man
    f.eks. kompilerer prosjektet i Eclipse. Fikset dette ved å bruke
    require() i nightwatch.conf.js

--- foundly/web-client/nightwatch.conf.js
+++ foundly/web-client/nightwatch.conf.js
@@ -2,7 +2,7 @@ module.exports = {
   webdriver: {
     start_process: true,
     port: 9515,
-    server_path: 'node_modules/.bin/chromedriver'
+    server_path: require('chromedriver').path
   },
 
   test_settings: {
